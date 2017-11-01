7029bdef740a88e66e9c4618c55ac262768c54f5
Sam Wood <solemankirby@gmail.com>
2017-11-01 16:26:43 -0400

    Completed step 1 and 2 of angular heroes project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+/*
+ *  Component Decorator:
+ *      requires a selector and template/templateUrl
+ * 
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "OVERWATCH";
+  hero: Hero = {id: 1, name: "Winston"};
 }
